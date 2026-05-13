1651c0f67caa8a7129115c5ba5b3a0ad3cd4182e
Fashiontex-IT01 <it1@fashiontex-bd.org>
2026-05-13 16:00:57 +0600

    Update
    ok

--- BMSAPI/BMSAPI/BusinessLayer/Interface/IBkashManager.cs
+++ BMSAPI/BMSAPI/BusinessLayer/Interface/IBkashManager.cs
@@ -6,10 +6,11 @@ namespace BMSAPI.BusinessLayer.Interface
     {
         // GET operations (Dapper)
         BkashBillInfo GetBillMonthWise(string UserName, string Password,string FlatCode, string BillMonth);
-        bool SaveBkashPayment(BkashPaymentRequest request);
+        //bool SaveBkashPayment(BkashPaymentRequest request);
+        public BkashBillPaymentResponse SaveBkashPayment(BkashPaymentRequest request);
 
-        BkashBillInfo GetBillByTrxId(string UserName, string Password, string TrxId);
+        //BkashBillInfo GetBillByTrxId(string UserName, string Password, string TrxId);
+        BkashBillPaymentResponse GetBillByTrxId(string UserName, string Password, string TrxId);
 
-        //public int UpdateBill(ViewModalBill entity);
     }
 }
