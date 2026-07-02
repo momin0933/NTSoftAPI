f30fe558a33f05908f4ca9ffb9ac53b3b6183c01
momin0933 <apon.cse09@gmail.com>
2026-07-03 01:01:20 +0600

    updated
    updated

--- InventoryAccountsAPI/Controllers/ItemController.cs
+++ InventoryAccountsAPI/Controllers/ItemController.cs
@@ -1,9 +1,11 @@
 ﻿using InventoryAccountsAPI.BusinessLayer.Interface;
 using InventoryAccountsAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FTSoftAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ItemController : ControllerBase
