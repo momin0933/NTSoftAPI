f2a36fb1bb3e84099141a4b938291408159370b2
KAMIL AHMED <130387705+Kamil-Ahmed67@users.noreply.github.com>
2026-08-17 13:57:32 +0600

    Update

---
+++ BMSAPI/BMSAPI/BusinessLayer/Interface/AppsInterface/ProHUB/IProperty.cs
@@ -0,0 +1,11 @@
+﻿using BMSAPI.Models.Apps.PropHUB;
+
+namespace BMSAPI.BusinessLayer.Interface.AppsInterface.ProHUB
+{
+    public interface IProperty
+    {
+        bool AddProperty(Property model);
+        IEnumerable<Property> GetPropertyList();
+        bool AddPropertyDetails(PropertyDetails model);
+    }
+}
---
+++ BMSAPI/BMSAPI/Models/Apps/PropHUB/PropertyDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace BMSAPI.Models.Apps.PropHUB
+{
+    public class PropertyDetails:Base
+    {
+        public string? Phone { get; set; }
+        public int? PropertyId { get; set; }
+        public string? FlatName { get; set; }
+        public string? Floor { get; set; }
+        public int? Room { get; set; }
+        public int? Bathroom { get; set; }
+        public int? Balcony { get; set; }
+        public string? MeterNo { get; set; }
+    }
+}
