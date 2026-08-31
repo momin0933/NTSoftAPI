662f010793ef90e499e06e70d986d6ee9f4cff34
Fashiontex-IT01 <it1@fashiontex-bd.org>
2026-08-31 17:34:06 +0600

    Update
    ok

--- BMSAPI/BMSAPI/BusinessLayer/Interface/SchoolInterface/ISchoolRegistration.cs
+++ BMSAPI/BMSAPI/BusinessLayer/Interface/SchoolInterface/ISchoolRegistration.cs
@@ -4,5 +4,15 @@ namespace BMSAPI.BusinessLayer.Interface.SchoolInterface
     public interface ISchoolRegistration
     {
         RegistrationResult RegisterStudent(SchoolUserRegistration model);
+
+        IEnumerable<SchoolUserRegistration> GetRegistrationReport(
+           DateTime? fromDate,
+           DateTime? toDate
+       );
+
+        // Registration By Id
+        SchoolUserRegistration GetRegistrationById(int id);
+
+        RegistrationResult UpdateRegistration(SchoolUserRegistration model);
     }
 }
